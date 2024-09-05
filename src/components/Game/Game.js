71e72291da32961388b65d6499aaff1e96ea75a3
Joshua M Smith <radjosh@gmail.com>
2024-09-05 13:45:56 -0400

    stretch 1 complete. keyboard with color coding

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
-import Input from '../Input'
-import Guesses from '../Guesses'
-import Keyboard from '../Keyboard';
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
+import Input from "../Input";
+import Guesses from "../Guesses";
+import Keyboard from "../Keyboard";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -17,35 +17,73 @@ function Game() {
   const [checks, setChecks] = React.useState([]);
   const [isWin, setIsWin] = React.useState(false);
   const [isLose, setIsLose] = React.useState(false);
-  const [keyboardState, setKeyboardState] = React.useState({Q: 'unused', W: 'unused'});
+  const [keyboardState, setKeyboardState] = React.useState(
+    _initializeKeyboardState
+  );
 
+  function _initializeKeyboardState() {
+    const alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    const initObj = {};
+    for (letter of alphabet) {
+      initObj[letter] = "unused";
+    }
+    return initObj;
+  }
 
   function handleGuess(guess) {
     const newGuess = {
-      text:guess,
-      id:crypto.randomUUID()
+      text: guess,
+      id: crypto.randomUUID(),
     };
     const nextGuesses = [...guesses, newGuess];
     setGuesses(nextGuesses);
     checkGuess(guess);
   }
 
+  function _updateKeyboard(updates) {
+    // iterate over changes, culling updatables into new keepers object
+    // use keepers to update state without mutation
+    // EX:
+    // updates['k']: 'misplaced'
+    let keepers = {};
+    for (let letter in updates) {
+      const oldStatus = keyboardState[updates[letter]];
+      if (oldStatus === "correct" || oldStatus === "incorrect") {
+        continue; // these never change
+      }
+      keepers[letter] = updates[letter];
+    }
+    setKeyboardState({
+      ...keyboardState,
+      ...keepers,
+    });
+  }
+
   function checkGuess(guess) {
+    // iterate over letters of current guess
+    // 2 effects:
+    // build new checks list of letter:status objects
+    // update keyboard w/ best statuses for each letter
     const newChecks = [];
     let isCorrect = true;
+    const updates = {};
 
     for (i = 0; i < guess.length; i++) {
       if (guess[i] === answer[i]) {
-        newChecks.push({ letter: guess[i], status: 'correct'})
+        newChecks.push({ letter: guess[i], status: "correct" });
+        updates[guess[i]] = "correct";
       } else if (answer.indexOf(guess[i]) > -1) {
-        newChecks.push({ letter: guess[i], status: 'misplaced'})
+        newChecks.push({ letter: guess[i], status: "misplaced" });
         isCorrect = false;
+        updates[guess[i]] = "misplaced";
       } else {
-        newChecks.push({ letter: guess[i], status: 'incorrect'})
-        isCorrect = false
+        newChecks.push({ letter: guess[i], status: "incorrect" });
+        isCorrect = false;
+        updates[guess[i]] = "incorrect";
       }
     }
     // console.log(newChecks);
+    _updateKeyboard(updates);
     setChecks([...checks, newChecks]);
     checkWin(isCorrect);
   }
@@ -53,7 +91,8 @@ function Game() {
   function checkWin(isCorrect) {
     if (isCorrect) {
       win();
-    } else if (guesses.length === NUM_OF_GUESSES_ALLOWED - 1) { // because of delay in rerendering!!
+    } else if (guesses.length === NUM_OF_GUESSES_ALLOWED - 1) {
+      // because of delay in rerendering!!
       lose();
     } else {
       return;
@@ -71,18 +110,18 @@ function Game() {
   }
 
   return (
-  <>
-    <Guesses guesses={guesses} checks={checks}/>
-    <Input 
-      handleGuess={handleGuess}
-      isWin={isWin}
-      isLose={isLose}
-      num_guesses={guesses.length}
-      answer={answer}
-    />
-    <Keyboard />
-  </>
-  )
+    <>
+      <Guesses guesses={guesses} checks={checks} />
+      <Input
+        handleGuess={handleGuess}
+        isWin={isWin}
+        isLose={isLose}
+        num_guesses={guesses.length}
+        answer={answer}
+      />
+      <Keyboard keyboardState={keyboardState} />
+    </>
+  );
 }
 
 export default Game;
--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,39 +1,40 @@
 import React from 'react';
 
-function Keyboard() {
+function Keyboard({keyboardState}) {
+  console.log(keyboardState);
   return (
     <div>
       <p className="keyboard">
-        <span className="keyboardCell blank">Q</span>
-        <span className="keyboardCell blank">W</span>
-        <span className="keyboardCell blank">E</span>
-        <span className="keyboardCell blank">R</span>
-        <span className="keyboardCell blank">T</span>
-        <span className="keyboardCell blank">Y</span>
-        <span className="keyboardCell blank">U</span>
-        <span className="keyboardCell blank">I</span>
-        <span className="keyboardCell blank">O</span>
-        <span className="keyboardCell blank">P</span>
+        <span className={"keyboardCell " + keyboardState['Q']}>Q</span>
+        <span className={"keyboardCell " + keyboardState['W']}>W</span>
+        <span className={"keyboardCell " + keyboardState['E']}>E</span>
+        <span className={"keyboardCell " + keyboardState['R']}>R</span>
+        <span className={"keyboardCell " + keyboardState['T']}>T</span>
+        <span className={"keyboardCell " + keyboardState['Y']}>Y</span>
+        <span className={"keyboardCell " + keyboardState['U']}>U</span>
+        <span className={"keyboardCell " + keyboardState['I']}>I</span>
+        <span className={"keyboardCell " + keyboardState['O']}>O</span>
+        <span className={"keyboardCell " + keyboardState['P']}>P</span>
       </p>
       <p className="keyboard">
-        <span className="keyboardCell blank">A</span>
-        <span className="keyboardCell blank">S</span>
-        <span className="keyboardCell blank">D</span>
-        <span className="keyboardCell blank">F</span>
-        <span className="keyboardCell blank">G</span>
-        <span className="keyboardCell blank">H</span>
-        <span className="keyboardCell blank">J</span>
-        <span className="keyboardCell blank">K</span>
-        <span className="keyboardCell blank">L</span>
+        <span className={"keyboardCell " + keyboardState['A']}>A</span>
+        <span className={"keyboardCell " + keyboardState['S']}>S</span>
+        <span className={"keyboardCell " + keyboardState['D']}>D</span>
+        <span className={"keyboardCell " + keyboardState['F']}>F</span>
+        <span className={"keyboardCell " + keyboardState['G']}>G</span>
+        <span className={"keyboardCell " + keyboardState['H']}>H</span>
+        <span className={"keyboardCell " + keyboardState['J']}>J</span>
+        <span className={"keyboardCell " + keyboardState['K']}>K</span>
+        <span className={"keyboardCell " + keyboardState['L']}>L</span>
       </p>
       <p className="keyboard">
-        <span className="keyboardCell blank">Z</span>
-        <span className="keyboardCell blank">X</span>
-        <span className="keyboardCell blank">C</span>
-        <span className="keyboardCell blank">V</span>
-        <span className="keyboardCell blank">B</span>
-        <span className="keyboardCell blank">N</span>
-        <span className="keyboardCell blank">M</span>
+        <span className={"keyboardCell " + keyboardState['Z']}>Z</span>
+        <span className={"keyboardCell " + keyboardState['X']}>X</span>
+        <span className={"keyboardCell " + keyboardState['C']}>C</span>
+        <span className={"keyboardCell " + keyboardState['V']}>V</span>
+        <span className={"keyboardCell " + keyboardState['B']}>B</span>
+        <span className={"keyboardCell " + keyboardState['N']}>N</span>
+        <span className={"keyboardCell " + keyboardState['M']}>M</span>
       </p>
     </div>
   )
